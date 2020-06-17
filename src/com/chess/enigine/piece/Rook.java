6dd705446a92db057b173cd69c92a4fe45813545
kimojono <oren.chakon@gmail.com>
2020-06-17 19:24:06 +0300

    queen done

--- src/com/chess/enigine/piece/Rook.java
+++ src/com/chess/enigine/piece/Rook.java
@@ -2,17 +2,50 @@ package com.chess.enigine.piece;
 
 import com.chess.enigine.Alliance;
 import com.chess.enigine.board.Board;
+import com.chess.enigine.board.BoardUtils;
 import com.chess.enigine.board.Move;
+import com.chess.enigine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Rook extends Piece{
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATE = {-8, -1, 1, 8};
     public Rook(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
     public Collection<Move> calculateMoves(Board board) {
-        return null;
+        List<Move> legalMoves = new ArrayList<>();
+        for (final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATE) {
+            int candidateDestinationCoordinate = this.piecePosition;
+            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if (isEdgeCaseException(candidateDestinationCoordinate,candidateCoordinateOffset)){break;}
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if (!candidateDestinationTile.isOccupied()) {
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAtDestinationAlliance = pieceAtDestination.getPieceAlliance();
+                        if (this.pieceAlliance != pieceAtDestinationAlliance) {
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate,
+                                    pieceAtDestination));
+                        }
+                        break;
+                    }
+
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+    private static boolean isEdgeCaseException(final int currentPosition,final int candidateOffset){
+        return ((BoardUtils.EIGHTH_COLUMN[currentPosition] && candidateOffset== 1)
+                || (BoardUtils.FIRST_COLUMN[currentPosition] && candidateOffset == -1));
     }
 }
