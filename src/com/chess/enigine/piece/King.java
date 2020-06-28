c7d75bc03c28fa37f6116806aeb4412b306006a0
kimojono <oren.chakon@gmail.com>
2020-06-28 23:01:44 +0300

    king done

--- src/com/chess/enigine/piece/King.java
+++ src/com/chess/enigine/piece/King.java
@@ -2,19 +2,48 @@ package com.chess.enigine.piece;
 
 import com.chess.enigine.Alliance;
 import com.chess.enigine.board.Board;
+import com.chess.enigine.board.BoardUtils;
 import com.chess.enigine.board.Move;
+import com.chess.enigine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class King extends Piece{
     private final static int[] CANDIDATE_MOVE_COORDINATE = {-9,-8,-7,-1,1,7,8,9};
 
-    public King(int piecePosition, Alliance pieceAlliance) {
+    public King(final int piecePosition,final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
-    public Collection<Move> calculateMoves(Board board) {
-        return null;
+    public Collection<Move> calculateMoves(final Board board) {
+        List<Move> legalMoves = new ArrayList<>();
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
+            final int candidateDestinationCoordinate = currentCandidateOffset+this.piecePosition;
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+                if (isEdgeException(this.piecePosition,currentCandidateOffset)){continue;}
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                if(!candidateDestinationTile.isOccupied()){
+                    legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                } else {
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                    final Alliance pieceAtDestinationAlliance = pieceAtDestination.getPieceAlliance();
+                    if (this.pieceAlliance != pieceAtDestinationAlliance){
+                        legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate,
+                                pieceAtDestination));
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+    private static boolean isEdgeException(final int currentPosition, final int candidateOffset){
+        return (BoardUtils.FIRST_COLUMN[currentPosition] &&
+                (candidateOffset == -9 || candidateOffset == -1 || candidateOffset == 7)) ||
+                (BoardUtils.EIGHTH_COLUMN[currentPosition] &&
+                (candidateOffset == -7 || candidateOffset == 1 || candidateOffset == 9));
     }
 }
